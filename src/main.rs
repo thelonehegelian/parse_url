bd7139ca357a6d95b674f0f933539523c049d23c
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 04:32:43 +0000

    Extract url origin, scheme, host etc

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
 
     fn build_url(base: Url, path: &str) -> Result<Url, ParseError> {
         let mut url = base;
+        // can also use join
         url.set_path(path);
         Ok(url)
     }
@@ -44,4 +45,14 @@ fn main() {
     let path = "/download/falloffeudalismi1904davi/falloffeudalismi1904davi.pdf";
     let new_url = build_url(base, path).unwrap();
     println!("{}", new_url);
+
+    /********************
+     * EXTRACT URL ORIGIN
+     ********************/
+
+    let url = Url::parse("https://archive.org/download/per_chicago-daily-tribune_1910-04-16_69_91/per_chicago-daily-tribune_1910-04-16_69_91.pdf").unwrap();
+    println!("{:?}", url.origin());
+    println!("{}", url.scheme());
+    println!("{}", url.host_str().unwrap());
+    println!("{}", url.port().unwrap_or_default());
 }
