5d96cbee5bbd2700028c2f02314671ae7d99fa8b
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 04:22:29 +0000

    Get base url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use url::{ParseError, Url};
+
 fn main() {
-    println!("Hello, world!");
+    /************************
+     * PARSE STRING TO A URL
+     ************************/
+    let s = "https://archive.org/download/chroniclesofcoun00grif/chroniclesofcoun00grif.pdf";
+    // make a url from a string
+    let url = Url::parse(s).unwrap();
+    println!("{}", url);
+
+    /******************
+     * CREATE BASE URL
+     *****************/
+
+    fn base_url(mut url: Url) -> Url {
+        match url.path_segments_mut() {
+            Ok(mut path) => {
+                path.clear();
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+        url
+    }
+
+    // create a base url remove paths
+    let url = Url::parse("https://archive.org/download/chroniclesofcoun00grif/").unwrap();
+    let base = base_url(url);
+    println!("{}", base);
 }
